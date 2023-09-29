4bd1fc05efc6f9dc3d516f1f60811a3eb5ee543a
Vladislav <kazakevich636@gmail.com>
2023-09-29 20:40:14 +0300

    Numbers in strings

--- codewars.js
+++ codewars.js
@@ -264,3 +264,15 @@ function basicOp(operation, value1, value2) {
 function litres(time) {
    return Math.floor(time * 0.5);
  }
+
+
+
+ // SOLUTION
+//  In this Kata, you will be given a string that has lowercase letters and numbers. Your task is to compare the number groupings and return the largest number. Numbers will not have leading zeros.
+//  For example, solve("gh12cdy695m1") = 695, because this is the largest of all number groupings.
+//  Good luck!
+
+// CONDITION
+function solve(s) {
+   return Math.max(...s.match(/\d+/g));
+ };
