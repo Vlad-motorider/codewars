6148b587325c9c21235d89ea674aa3f32b3a2895
Vladislav <kazakevich636@gmail.com>
2023-09-30 21:46:07 +0300

    Volume of a Cuboid

--- codewars.js
+++ codewars.js
@@ -297,3 +297,15 @@ function solve(s) {
 String.prototype.isUpperCase = function() {
    return this.toString() === this.toUpperCase()
 }
+
+
+
+// SOLUTION
+// Bob needs a fast way to calculate the volume of a cuboid with three values: the length, width and height of the cuboid. Write a function to help Bob with this calculation.
+
+// CONDITION
+class Kata {
+   static getVolumeOfCuboid(length, width, height) {
+     return length * width * height;
+   }
+}
