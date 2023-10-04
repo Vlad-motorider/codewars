295bb5f4be78d57db70153993b70dbe14e84243b
Vladislav <kazakevich636@gmail.com>
2023-10-04 20:39:02 +0300

    Grasshopper - Summation

--- codewars.js
+++ codewars.js
@@ -388,3 +388,18 @@ function opposite(number) {
 function removeChar(str) {
    return str.split('').slice(1, str.length - 1).join('');
 }
+
+
+
+// SOLUTION
+// Write a program that finds the summation of every number from 1 to num. The number will always be a positive integer greater than 0.
+
+// CONDITION
+function summation(num) {
+   let result = 0
+   for (let i = 1; i <= num; i++) {
+      result += i;
+   } 
+   return result
+}
+
