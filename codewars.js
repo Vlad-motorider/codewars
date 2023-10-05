44b42ea5cb09798a6e54b4287081220fcaf4f432
Vladislav <kazakevich636@gmail.com>
2023-10-05 15:56:13 +0300

    Shortest Word

--- codewars.js
+++ codewars.js
@@ -444,3 +444,14 @@ function greet(name){
 function makeUpperCase(str) {
    return str.toUpperCase();
 }
+
+
+
+// SOLUTION
+// Simple, given a string of words, return the length of the shortest word(s).
+// String will never be empty and you do not need to account for different data types.
+
+// CONDITION
+function findShort(s){
+   return Math.min(...s.split(" ").map (s => s.length));
+}
