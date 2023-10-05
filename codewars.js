7160d690e824d0ce917301003c3fb9d5aca0074d
Vladislav <kazakevich636@gmail.com>
2023-10-05 15:27:25 +0300

    Returning Strings

--- codewars.js
+++ codewars.js
@@ -424,3 +424,13 @@ function noSpace(x){
    return x.split(' ').join('');
 }
 
+
+
+// SOLUTION
+// Make a function that will return a greeting statement that uses an input; your program should return, "Hello, <name> how are you doing today?".
+//[Make sure you type the exact thing I wrote or the program may not execute properly]
+
+// CONDITION
+function greet(name){
+   return name = `Hello, ${name} how are you doing today?`;
+}
