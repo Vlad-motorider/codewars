b3c0952caa57eb47e472d5ebb36a75a6bb5e503c
Vladislav <kazakevich636@gmail.com>
2023-10-08 19:41:59 +0300

    Reversing Words in a String

--- codewars.js
+++ codewars.js
@@ -498,3 +498,18 @@ function abbrevName(name){
 function booleanToString(b) {
    return b ? 'true' : 'false'
 }
+
+
+
+// SOLUTION
+// You need to write a function that reverses the words in a given string. A word can also fit an empty string. If this is not clear enough, here are some examples:
+// As the input may have trailing spaces, you will also need to ignore unneccesary whitespace.
+//Example (Input --> Output)
+//"Hello World" --> "World Hello"
+//"Hi There." --> "There. Hi"
+//Happy coding!
+
+// CONDITION
+function reverse(string){
+   return string.split(' ').reverse().join(' ');
+}
