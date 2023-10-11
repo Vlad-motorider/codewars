c443b75a9e98604d27f4ce78739c207e55541205
Vladislav <kazakevich636@gmail.com>
2023-10-11 21:41:34 +0300

    Opposites Attract

--- codewars.js
+++ codewars.js
@@ -533,3 +533,19 @@ function simpleMultiplication(number) {
 function digitize(n) {
    return String(n).split('').reverse().map(char => Number(char));
 }
+
+
+
+// SOLUTION
+// Timmy & Sarah think they are in love, but around where they live, they will only know once they pick a flower each.
+// If one of the flowers has an even number of petals and the other has an odd number of petals it means they are in love.
+// Write a function that will take the number of petals of each flower and return true if they are in love and false if they aren't.
+
+// CONDITION
+function lovefunc(flower1, flower2){
+   if(flower1 % 2 !== flower2 % 2) {
+      return true;
+   } else {
+      return false;
+   }
+}
