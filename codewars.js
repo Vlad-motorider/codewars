fa5c5c724275e9088c252ba0f141df637dba424f
Vladislav <kazakevich636@gmail.com>
2023-10-15 21:52:05 +0300

    Fake Binary

--- codewars.js
+++ codewars.js
@@ -566,3 +566,14 @@ function findNeedle(haystack) {
    const index = haystack.findIndex(junk => junk === 'needle');
    return `found the needle at position ${index}`;
 }
+
+
+
+// SOLUTION
+//Given a string of digits, you should replace any digit below 5 with '0' and any digit 5 and above with '1'. Return the resulting string.
+//Note: input will never be an empty string
+
+// CONDITION
+function fakeBin(x){
+   return x.split('').map(num => (Number(num) >= 5 ? 1 : 0)).join('');
+}
