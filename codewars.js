9b7f1123ca8f011fc11c48552d911880b0742964
Vladislav <kazakevich636@gmail.com>
2023-10-17 19:23:34 +0300

    Beginner Series #2 Clock

--- codewars.js
+++ codewars.js
@@ -604,3 +604,27 @@ function smash (words) {
 function maps(x){
    return x.map(number => number * 2);
 }
+
+
+
+// SOLUTION
+// Clock shows h hours, m minutes and s seconds after midnight.
+// Your task is to write a function which returns the time since midnight in milliseconds.
+// Example:
+// h = 0
+// m = 1
+// s = 1
+// result = 61000
+// Input constraints:
+// 0 <= h <= 23
+// 0 <= m <= 59
+// 0 <= s <= 59
+
+// CONDITION
+function past(h, m, s){
+   let hours = h * 3600000;
+   let minutes = m * 60000;
+   let seconds = s * 1000;
+   
+   return hours + minutes + seconds;
+}
