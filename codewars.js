f47b51ec1683ac20a27a19c882f23556401a2528
Vladislav <kazakevich636@gmail.com>
2023-10-18 21:34:05 +0300

    Reversed Words

--- codewars.js
+++ codewars.js
@@ -645,3 +645,19 @@ function areYouPlayingBanjo(name) {
 
    return `${name} does not play banjo`;
 }
+
+
+
+// SOLUTION
+// Complete the solution so that it reverses all of
+//  the words within the string passed in.
+// Words are separated by exactly one space
+//  and there are no leading or trailing spaces.
+// Example(Input --> Output):
+// "The greatest victory is that which requires no battle" --> 
+// "battle no requires which that is victory greatest The"
+
+// CONDITION
+function reverseWords(str){
+   return str.split(' ').reverse().join(' '); // reverse those words
+}
